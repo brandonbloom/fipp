6ffd94d9a6f9fa4fa8318af75c27d79d40b898fc
Brandon Bloom <brandon@brandonbloom.name>
2013-01-09 02:34:47 -0800

    State Transducers via clojure.core.reducers

---
+++ src/pprint/transduce.clj
@@ -0,0 +1,56 @@
+(ns pprint.transduce
+  (:require [clojure.core.reducers :as r]))
+
+(defmacro defcurried
+  "Builds another arity of the fn that returns a fn awaiting the last param"
+  [name doc args & body]
+  (let [cargs (vec (butlast args))]
+    `(defn ~name ~doc
+       (~cargs (fn [x#] (~name ~@cargs x#)))
+       (~args ~@body))))
+
+(defcurried map-state
+  "Like map, but threads a state through the sequence of transformations.
+  For each x in coll, f is applied to [state x] and should return [state' x'].
+  The first invocation of f uses init as the state."
+  [f init coll]
+  (r/reducer coll
+    (fn [f1]
+      (let [state (atom init)]
+        (fn [acc x]
+          (let [[state* x*] (f @state x)]
+            (reset! state state*)
+            (f1 acc x*)))))))
+
+(defcurried mapcat-state
+  "Like mapcat, but threads a state through the sequence of transformations.
+  For each x in coll, f is applied to [state x] and should return [state' xs].
+  The result is the concatenation of each returned xs."
+  [f init coll]
+  (r/reducer coll
+    (fn [f1]
+      (let [state (atom init)]
+        (fn [acc x]
+          (let [[state* xs] (f @state x)]
+            (reset! state state*)
+            (if (seq xs)
+              (reduce f1 acc xs)
+              acc)))))))
+
+(comment
+
+  (->> (range 5)
+    (map-state (fn [index x]
+                 [(inc index) (/ x index)])
+               1)
+    (into []))
+
+  (->> (range 10)
+    (mapcat-state (fn [buffer x]
+                     (if (< (count buffer) 5)
+                       [(conj buffer x) nil]
+                       [[] buffer]))
+                  [])
+    (into []))
+
+)
