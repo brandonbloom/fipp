afb5090acc7bba9bdcb38f746b100fd7a5453b03
Brandon Bloom <brandon@brandonbloom.name>
2013-01-11 15:24:48 -0800

    Another benchmark

--- test/fipp/benchmark.clj
+++ test/fipp/benchmark.clj
@@ -2,12 +2,36 @@
   (:require [clojure.pprint]
             [bbloom.fipp.edn]))
 
-(defn bench [f x]
+(defn bench [f n x]
   (time
-    (dotimes [i 100]
+    (dotimes [i n]
       (with-out-str
         (f x)))))
 
-(def x (vec (range 1000)))
-(bench clojure.pprint/pprint x)  ; 13389.499 msecs
-(bench bbloom.fipp.edn/pprint x) ;  5111.764 msecs
+(defn compare [n x]
+  (bench clojure.pprint/pprint n x)
+  (bench bbloom.fipp.edn/pprint n x))
+
+(compare 100 (vec (range 1000)))
+"Elapsed time: 12827.101 msecs"
+"Elapsed time:  4505.121 msecs"
+
+
+(defn random-value []
+  (let [f (rand-nth [identity keyword symbol])
+        x (rand-nth ["foo" "bar" "baz"])]
+    (f x)))
+
+(defn random-seq []
+  (let [n (+ (rand-int 5) 2)]
+    (repeatedly n random-value)))
+
+(defn random-map []
+  (reduce (fn [m [v & path]]
+            (assoc-in m path v)) ; this sometimes fails, come up with some better random-map
+          {}
+          (repeatedly 5 random-seq)))
+
+(compare 1000 (random-map))
+"Elapsed time: 4412.506 msecs"
+"Elapsed time: 2732.823 msecs"
