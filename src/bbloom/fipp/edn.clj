50ba446dad7f187217775dce5de371e8f8a91fde
Brandon Bloom <brandon@brandonbloom.name>
2013-01-11 19:12:44 -0800

    Support records

--- src/bbloom/fipp/edn.clj
+++ src/bbloom/fipp/edn.clj
@@ -14,11 +14,19 @@
 (defmethod pretty clojure.lang.ISeq [s]
   [:group "(" [:align (interpose :line (map pretty s))] ")"])
 
-(defmethod pretty clojure.lang.IPersistentMap [m]
+(defn pretty-map [m]
   (let [kvps (map (fn [[k v]]
                     [:span (pretty k) " " (pretty v)])
                   m)]
     [:group "{" [:align (interpose [:span "," :line] kvps)]  "}"]))
+
+(defmethod pretty clojure.lang.IPersistentMap [m]
+  (pretty-map m))
+
+(defmethod pretty clojure.lang.IRecord [r]
+  [:span "#" (-> r class .getName) (pretty-map r)])
+
+(prefer-method pretty clojure.lang.IRecord clojure.lang.IPersistentMap)
 
 (defmethod pretty clojure.lang.IPersistentSet [s]
   [:group "#{" [:align (interpose :line (map pretty s))] "}"])
@@ -31,12 +39,15 @@
 
 (comment
 
+  (defrecord Person [first-name last-name])
+
   (->
     ;(list 1 2 3 4 [:a :b :c :d] 5 6 7 8 9)
     ;{:foo 1 :bar \c :baz "str"}
-    {:small-value [1 2 3]
-     :larger-value {:some-key "foo"
-                    :some-other-key "bar"}}
-    (pprint {:width 12}))
+    ;{:small-value [1 2 3]
+    ; :larger-value {:some-key "foo"
+    ;                :some-other-key "bar"}}
+    (Person. "Brandon" "Bloom")
+    (pprint {:width 60}))
 
 )
