150911218734cb2017d37d0b8469f5d5b365f367
Brandon Bloom <brandon@brandonbloom.name>
2013-03-22 01:11:04 -0400

    Upgrade to Clojure 1.5.1

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/brandonbloom/fipp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.finger-tree "0.0.1"]])
