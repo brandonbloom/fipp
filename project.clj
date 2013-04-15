a981c353efed68c4cba7484142e94fbfed70346b
Brandon Bloom <brandon@brandonbloom.name>
2013-04-14 23:01:51 -0400

    Bump version.
    multimethod -> protocol is a breaking change for edn dispatch extensions
    
    That probably affects near-zero people...

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fipp "0.1.0-SNAPSHOT"
+(defproject fipp "0.2.0-SNAPSHOT"
   :description "Fast Idiomatic Pretty Printer for Clojure"
   :url "https://github.com/brandonbloom/fipp"
   :license {:name "Eclipse Public License"
