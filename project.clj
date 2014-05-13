e96cd70e2feee50bc03de5dc43bc1f9bd2e798e5
Brandon Bloom <brandon@brandonbloom.name>
2014-05-13 13:32:58 -0400

    version 0.4.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fipp "0.4.1"
+(defproject fipp "0.4.2"
   :description "Fast Idiomatic Pretty Printer for Clojure"
   :url "https://github.com/brandonbloom/fipp"
   :license {:name "Eclipse Public License"
