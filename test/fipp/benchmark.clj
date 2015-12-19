43736b5c03e4df5d624bd02f00cb1eff6c415cc9
Michael Griffiths <mikey@cich.li>
2015-12-19 03:04:13 +0000

    Use generated data for benchmarking

--- test/fipp/benchmark.clj
+++ test/fipp/benchmark.clj
@@ -1,26 +1,42 @@
 (ns fipp.benchmark
-  (:require [clojure.pprint]
+  (:require [fipp.clojure]
             [fipp.edn]
-            [fipp.clojure]
-            [criterium.core :refer [bench]]))
+            [clojure.java.io :as io]
+            [clojure.pprint]
+            [clojure.test.check.generators :as gen]
+            [clojure.tools.reader.edn :as edn]
+            [criterium.core :refer [bench]])
+  (:import [java.io File Writer]))
 
-(def benched-fns [
-  ;prn
-  fipp.edn/pprint
-  fipp.clojure/pprint
-  ;clojure.pprint/pprint
-])
+(set! *warn-on-reflection* true)
 
-(def samples {
-  :long (vec (range 10000))
-  ;; Dirty hacky source of test data:
-  :mixed (read-string {:read-cond :allow} (str "[" (slurp "src/fipp/engine.cljc") "]"))
-})
+(def benched-fns [prn
+                  fipp.edn/pprint
+                  fipp.clojure/pprint
+                  clojure.pprint/pprint])
+
+(def writer
+  (proxy [Writer] []
+    (write [_])
+    (flush [])
+    (close [])))
+
+(defn samples []
+  (let [^File file (io/file ".benchmark-samples")]
+    (when-not (.exists file)
+      (spit file (with-out-str
+                   (fipp.edn/pprint (gen/sample gen/any-printable 1000)))))
+
+    (edn/read-string (slurp file))))
 
 ;; lein run -m fipp.benchmark
 (defn -main []
-  (doseq [f benched-fns
-          [k v] samples]
-    (println "Testing " f " on " k)
-    (criterium.core/with-progress-reporting
-      (bench (with-out-str (f v))))))
+  (let [samples (samples)]
+    (doseq [f benched-fns]
+      (println "Benchmarking " f)
+      (time
+       (bench
+        (binding [*out* writer]
+          (doseq [sample samples]
+            (f sample)))))
+      (println))))
