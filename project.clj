181ceb4238146feb2a16e02220829d6616948f13
Brandon Bloom <brandon@brandonbloom.name>
2015-12-22 18:18:48 -0800

    Use clj 1.7 proper

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/brandonbloom/fipp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.rrb-vector "0.0.11"]]
   :profiles {:dev {:dependencies [[criterium "0.4.3"]]}})
