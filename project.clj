2abc90f63578c052ea9958bf3093cb7430b3d898
Brandon Bloom <brandon@brandonbloom.name>
2019-04-14 17:38:07 -0700

    v0.6.18 fixes warning in cljs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fipp "0.6.17"
+(defproject fipp "0.6.18"
   :description "Fast Idiomatic Pretty Printer for Clojure"
   :url "https://github.com/brandonbloom/fipp"
   :license {:name "Eclipse Public License"
