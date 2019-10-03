2534620d5a942851e09679af6a6806eac1b8ad4e
Brandon Bloom <brandon@brandonbloom.name>
2019-10-02 21:27:13 -0700

    Upgrade rrb-vector to 0.1.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.rrb-vector "0.0.14"]]
+                 [org.clojure/core.rrb-vector "0.1.0"]]
   :profiles {:dev {:dependencies [[criterium "0.4.3"]
                                   [org.clojure/clojurescript "1.9.854"]]}}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
