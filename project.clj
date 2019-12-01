c88ba1128df184358d72e2a6edc8daec8a96c7b1
Brandon Bloom <brandon@brandonbloom.name>
2019-12-01 11:53:18 -0800

    v0.6.22
    Fixes #71.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject fipp "0.6.21"
+(defproject fipp "0.6.22"
   :description "Fast Idiomatic Pretty Printer for Clojure"
   :url "https://github.com/brandonbloom/fipp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.rrb-vector "0.1.0"]]
+                 [org.clojure/core.rrb-vector "0.1.1"]]
   :profiles {:dev {:dependencies [[criterium "0.4.3"]
                                   [org.clojure/clojurescript "1.9.854"]
                                   [javax.xml.bind/jaxb-api "2.3.1"]]}}
