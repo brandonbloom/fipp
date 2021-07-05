4c64e16ce4f2d8d4e59329a9917b1fcb8274e591
Brandon Bloom <brandon@brandonbloom.name>
2021-07-04 20:01:55 -0700

    follow github actions and env var naming conventions

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.rrb-vector "0.1.1"]]
-  :eval-in-leiningen ~(= "true" (System/getenv "eval_in_leiningen"))
+  ;; See <https://github.com/brandonbloom/fipp/issues/72#issuecomment-576816826>
+  ;; and test code in <.github/workflows/pr.yml>.
+  :eval-in-leiningen ~(= "true" (System/getenv "EVAL_IN_LEININGEN"))
   :profiles {:dev {:dependencies [[criterium "0.4.3"]
                                   [org.clojure/clojurescript "1.9.854"]
                                   [javax.xml.bind/jaxb-api "2.3.1"]]}}
